77300d00bfc9751066d80d33a2687d2d7dc33f69
brian <brian.sjoberg@excella.com>
2021-11-30 17:00:33 -0500

    Organizing step defs.

--- features/steps/hello_world_steps.py
+++ features/steps/hello_world_steps.py
@@ -11,22 +11,22 @@ class HelloWorldTestCase(LiveServerTestCase):
     def step_impl(self):
         assert HelloWorld() is not None
 
-    @given(u'I have a web server running')
-    def step_impl(self):
-        self.result = webdriver.Chrome()
-
     @when(u'I say hi')
     def step_impl(self):
         greeter = HelloWorld()
         self.result = greeter.say_hi()
 
-    @when(u'I go to the hello world page')
-    def step_impl(self):
-        self.result.get('http://localhost:8000/hello_world')
-
     @then(u'I hear "{expected_result}"')
     def step_impl(self, expected_result):
         assert expected_result == self.result, 'Expect {} but got {}'.format(expected_result, self.result)
+
+    @given(u'I have a web server running')
+    def step_impl(self):
+        self.result = webdriver.Chrome()
+
+    @when(u'I go to the hello world page')
+    def step_impl(self):
+        self.result.get('http://localhost:8000/hello_world')
 
     @then(u'I see "{expected_result}" in the title')
     def step_impl(self, expected_result):
